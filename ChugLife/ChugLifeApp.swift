c59ea3d33afe2a9052256f42a7676f2a6a067778
Arda <rardayurtkuran@gmail.com>
2025-01-27 14:20:14 +0300

    Initial Commit

---
+++ ChugLife/ChugLifeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChugLifeApp.swift
+//  ChugLife
+//
+//  Created by Arda Yurtkuran on 27.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct ChugLifeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChugLifeTests/ChugLifeTests.swift
@@ -0,0 +1,17 @@
+//
+//  ChugLifeTests.swift
+//  ChugLifeTests
+//
+//  Created by Arda Yurtkuran on 27.01.2025.
+//
+
+import Testing
+@testable import ChugLife
+
+struct ChugLifeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
